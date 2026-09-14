e5782be4c6f3783283c49a8dcb949f8d90021fb7
sashavenskyi333 <vealex099@gmail.com>
2026-09-14 08:46:02 +0200

    Add files via upload

---
+++ Зарплата Venska easy body.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 07:45:47 2026
+
+@author: olve
+"""
+
+import sys, subprocess
+subprocess.check_call([sys.executable, "-m", "pip", "install", "customtkinter", "pandas", "openpyxl"])
